9aab4bd4e67a7becfa23bc104e93fae3d4c5c39c
Drew Csillag <drew@thecsillags.com>
2020-12-22 12:53:44 -0500

    added centerline letter -- is probably one line too many as it gets cut off

---
+++ letter-centerline/notepaper.py
@@ -0,0 +1,36 @@
+import sys
+
+
+print """<svg height="1100" width="850">"""
+offset = 11
+
+hundint = (offset/.2540)
+
+margin = 8.5/.254
+
+while hundint < 1075.0:
+    sys.stderr.write("hundint %f\n" % hundint)
+    print """<line x1="%f" y1="%f" x2="%f" y2="%f" style="stroke: rgb(200,200,200); stroke-width:%f" />""" % (
+        margin, hundint, 550 - margin, hundint, 1.5625 / 2)
+    print """<line x1="%f" y1="%f" x2="%f" y2="%f" style="stroke: rgb(200,200,200); stroke-width:%f" />""" % (
+        margin, hundint, 850 - margin, hundint, 1.5625 / 2)
+
+    offset += 6.5
+    hundint = (offset/.254) 
+
+print """<line x1="%f" y1="%f" x2="%f" y2="%f" style="stroke: rgb(200,200,200); stroke-width:%f" />""" % (
+    margin, 11/.254,
+    margin, (offset - 6.5) / .254,
+    1.5625 / 2)
+
+print """<line x1="%f" y1="%f" x2="%f" y2="%f" style="stroke: rgb(200,200,200); stroke-width:%f" />""" % (
+    850 - margin, 11/.254,
+    850 - margin, (offset - 6.5) / .254,
+    1.5625 / 2)
+
+print """<line x1="%f" y1="%f" x2="%f" y2="%f" style="stroke: rgb(200,200,200); stroke-width:%f" />""" % (
+    425, 11/.254,
+    425, (offset - 6.5) / .254,
+    1.5625 / 2)
+
+print """</svg>"""
